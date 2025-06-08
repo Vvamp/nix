ebba009cf6033201aad0d81d91a8b9b45e6fd7de
Vvamp <vvampus@gmail.com>
2025-06-08 16:01:51 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Vvamp's NixOS Config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, home-manager }: 
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./hosts/default/configuration.nix
+        ];
+      };
+    });
+}
